ef3fa804fb7dc7b1a76c065fbe3dfb2a11f64a27
Tony Kordysh <tkordysh@gmail.com>
2022-05-28 09:44:52 -0400

    restrict middleware built

---
+++ api/middleware/auth-middleware.js
@@ -0,0 +1,33 @@
+const { JWT_SECRET } = require('../secrets')
+const jwt = require('jsonwebtoken')
+
+const restrict = (req, res, next) => {
+  const token = req.headers.authorization
+  if (!token) {
+    return next({ status: 401, message: "token required" })
+  }
+  jwt.verify(token, JWT_SECRET, (err, decoded) => {
+    if(err) {
+      next({ status: 401, message: "token invalid" })
+    } else {
+      next()
+    }
+  })
+ 
+  /*
+    IMPLEMENT
+
+    1- On valid token in the Authorization header, call next.
+
+    2- On missing token in the Authorization header,
+      the response body should include a string exactly as follows: "token required".
+
+    3- On invalid or expired token in the Authorization header,
+      the response body should include a string exactly as follows: "token invalid".
+  */
+};
+
+
+module.exports = {
+  restrict,
+}
--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 
-const restrict = require('./middleware/restricted.js');
+const { restrict } = require('./middleware/auth-middleware')
 
 const authRouter = require('./auth/auth-router.js');
 const jokesRouter = require('./jokes/jokes-router.js');
@@ -15,5 +15,12 @@ server.use(express.json());
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', restrict, jokesRouter); // only logged-in users should have access!
+
+server.use((err, req, res, next) => { // eslint-disable-line
+    res.status(err.status || 500).json({
+      message: err.message,
+      stack: err.stack,
+    });
+  });
 
 module.exports = server;
